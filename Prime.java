b53dc50579f2770844aa888eee0716af4f70d077
Nighthawk <luks.schroeter@gmail.com>
2022-11-02 17:09:21 +0100

    commit

--- Prime.java
+++ Prime.java
@@ -32,5 +32,32 @@ public class Sieb {
         }
 
     }
+    public static void printprimes(int n){
+        int[] array = new int[n+1];
+        for (int i = 2; i < n +1; i++) {
+            array[i] = i;
+            //System.out.println(i);
+        }
+        for (int i = 2; i < Math.sqrt(n); ++i) {
+
+            for (int j = 0; j < array.length; j++) {
+                if (i != array[j]) {
+                    if (array[j] % i == 0) {
+                        array[j] = 0;
+                    }
+                }
+            }
+
+        }
+        for (int i = 0; i < array.length; ++i) {
+            if (array[i] != 0) {
+               System.out.println(array[i]);
+            } else {
+                continue;
+            }
+        }
+        
+    }
+
     
 }
